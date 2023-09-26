6914de469bd48711c5ae8115c832bbe0edc67819
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-09-26 22:20:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-           Quiz App
+           Quiz App Demo
         </a>
       </header>
     </div>
